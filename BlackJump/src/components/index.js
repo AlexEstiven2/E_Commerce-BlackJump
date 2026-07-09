f7bc2d399206e9a612dc416e0328411b73f0a9bb
DevJhojan <jhojan1806@gmail.com>
2026-07-08 19:03:47 -0500

    Agregar configuración de jsconfig y alias para componentes en Vite

---
+++ BlackJump/src/components/index.js
@@ -0,0 +1,4 @@
+export {default as BarraLateralInferior} from './BarraLateralInferior';
+export {default as BarraLateralSuperior} from './BarraLateralSuperior';
+export {default as Carrusel} from './Carrusel';
+export {default as Encabezado} from './Encabezado';
